d3638b6a150b3803f237ecc2ebcf59397ab14692
SametTurkey <sametturkeytr@gmail.com>
2017-12-30 14:24:25 +0300

    Cube

--- Cube.js
+++ Cube.js
@@ -645,14 +645,12 @@ bot.on("message", function(message) {
                                 msg.edit("<:yazi:389159538957352962>")
                                 delay(500).then(() => {
                                     msg.edit("<:tura:389159536277323776>")
-                                    delay(500).then(() => {
-                                        if (cevaplar[Math.floor(Math.random() * cevaplar.length)] == "Yazı") {
-                                            msg.edit("<:yazi:389159538957352962> **| Sonuç: Yazı**")
-                                        }
-                                        else if (cevaplar[Math.floor(Math.random() * cevaplar.length)] == "Tura") {
-                                            msg.edit("<:tura:389159536277323776> **| Sonuç: Tura**")
-                                        }
-                                    });
+                                    if (cevaplar[Math.floor(Math.random() * cevaplar.length)] == "Yazı") {
+                                        msg.edit("<:yazi:389159538957352962> **| Sonuç: Yazı**")
+                                    }
+                                    else if (cevaplar[Math.floor(Math.random() * cevaplar.length)] == "Tura") {
+                                        msg.edit("<:tura:389159536277323776> **| Sonuç: Tura**")
+                                    }
                                 });
                             });
                         });
