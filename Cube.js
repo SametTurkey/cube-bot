5e973447f59e748b50e902a320472abc59073a62
SametTurkey <sametturkeytr@gmail.com>
2018-01-06 22:26:58 +0300

    Cube

--- Cube.js
+++ Cube.js
@@ -309,22 +309,21 @@ bot.on("message", function(message) {
                 var steamgames = 0
                 request.get("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=D007A91AEECB430CED9666E886056870&vanityurl=" + steam, {host: "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=D007A91AEECB430CED9666E886056870&vanityurl=" + steam}, function(err,res,body) {
                     var responseid = JSON.parse(body)
-                    if (!responseid.response.success == 42) {
+                    if (responseid.response.success == 1) {
                         steamsuccess = true
                         steamid = responseid.response.steamid
-                    }
-                    if (steamsuccess == true) {
-                        request.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=D007A91AEECB430CED9666E886056870&steamids=" + steamid, {host: "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=D007A91AEECB430CED9666E886056870&steamids=" + steamid}, function(err,res,body) {
-                            var responsekullanici = JSON.parse(body)
-                            steamusername = responsekullanici.response.players[steamid]
-                            steamavatar = responsekullanici.response.players[avatarmedium]
-                        });
-                        request.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=D007A91AEECB430CED9666E886056870&steamid=" + steamid + "&format=json", {host: "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=D007A91AEECB430CED9666E886056870&steamid=" + steamid + "&format=json"}, function(err,res,body) {
-                            var responseoyun = JSON.parse(body)
-                            steamgames = responseoyun.response.game_count
-                        });
-                    }
                 });
+                if (steamsuccess == true) {
+                    request.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=D007A91AEECB430CED9666E886056870&steamids=" + steamid, {host: "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=D007A91AEECB430CED9666E886056870&steamids=" + steamid}, function(err,res,body) {
+                        var responsekullanici = JSON.parse(body)
+                        steamusername = responsekullanici.response.players[steamid]
+                        steamavatar = responsekullanici.response.players[avatarmedium]
+                    });
+                    request.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=D007A91AEECB430CED9666E886056870&steamid=" + steamid + "&format=json", {host: "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=D007A91AEECB430CED9666E886056870&steamid=" + steamid + "&format=json"}, function(err,res,body) {
+                        var responseoyun = JSON.parse(body)
+                        steamgames = responseoyun.response.game_count
+                    });
+                }
                 var embed = new Discord.RichEmbed()
                     .setAuthor(steamusername, steamavatar)
                     .addField("Oyun Sayısı", steamgames)
@@ -333,6 +332,7 @@ bot.on("message", function(message) {
                     .setFooter("Cube | SametTurkey#0286 | " + new Date())
                 message.channel.send(embed);
             }
+            break
         case "google":
             if (!args[1] == "") {
                 var googlearama = args.join(" ").replace("google", "").replace("undefined", "")
