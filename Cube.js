ac9bf7f83d4e8aa6750bb8f23532872af6716601
SametTurkey <sametturkeytr@gmail.com>
2018-01-08 22:44:12 +0300

    Cube

--- Cube.js
+++ Cube.js
@@ -96,10 +96,10 @@ function sleep(millis) {
 bot.on("message", function(message) {
     if (message.author.equals(bot.user)) return;
 
-    if (message.content.startsWith(Prefix) || message.content.startsWith("@" + bot.user.username + "#5681")) {
+    if (message.content.startsWith(Prefix) || message.content.startsWith("@" + bot.user.username + "#" + bot.user.discriminator)) {
     try {
 
-    var args = message.content.substring(Prefix.length).substring("@" + bot.user.username + "#5681").split(" ")
+    var args = message.content.substring(Prefix.length).substring("@" + bot.user.username + "#" + bot.user.discriminator).split(" ")
 
     switch (args[0].toLowerCase()) {
         case "adminduyuru":
