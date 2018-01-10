6efa2755ab9ff5675d5f029a70d44da111a0eb3f
SametTurkey <sametturkeytr@gmail.com>
2018-01-11 00:13:45 +0300

    Cube

--- Cube.js
+++ Cube.js
@@ -209,7 +209,7 @@ bot.on("message", function(message) {
             throw new Error("İsteğe bağlı hata verdirildi!")
             break
 	case "emoji":
-	    var emoji = args.join(" ").replace("emoji").replace(" ", "").replace("undefined", "").replace(new RegExp("a"), ":regional_indicator_a: ").replace(new RegExp("b"), ":regional_indicator_b: ").replace(new RegExp("c"), ":regional_indicator_c: ")
+	    var emoji = args.join(" ").replace("emoji").replace(" ", "").replace("undefined", "")
 	    message.channel.send(emoji);
 	    break
         case "bilgi":
